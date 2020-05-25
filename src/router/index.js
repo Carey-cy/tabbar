50494649990d73232a575d493ed7ed99f2d5ffa3
Carey-cy <changanazai@163.com>
2020-05-25 09:21:09 +0800

    little demo

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+Vue.use(Router)
+
+const Home = () => import('views/home/Home.vue')
+const Category = () => import('views/category/Category.vue')
+const Cart = () => import('views/cart/Cart.vue')
+const Profile = () => import('views/profile/Profile.vue')
+
+const routes= [
+  {
+    path:'',
+    redirect:'/home'
+  },
+  {
+    path:'/category',
+    component:Category
+  },
+  {
+    path:'/cart',
+    component:Cart
+  },
+  {
+    path:'/profile',
+    component:Profile
+  },
+  {
+    path:'/home',
+    component:Home
+  }
+]
+export default new Router({
+  routes,
+  mode:'history'
+})
